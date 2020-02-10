68cd434e20260f9354c6f1d9bdef84a6898be746
AlexisRuiz00 <alexisruizgomez96@gmail.com>
2020-02-10 21:54:23 +0100

    Servidor SMPT TAREAS 5 y 6

---
+++ src/SERVICIOS/SMTP/Ejemplo1POP3.java
@@ -0,0 +1,39 @@
+package SERVICIOS.SMTP;
+
+import org.apache.commons.net.pop3.POP3Client;
+import org.apache.commons.net.pop3.POP3MessageInfo;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+public class Ejemplo1POP3 {
+
+
+    public static void main(String[] args) {
+
+        String server = "localhost", username = "usu1", password="usu1";
+        int puerto = 110;
+
+        POP3Client pop3 = new POP3Client();
+        try {
+            pop3.connect(server,puerto);
+            System.out.println("Conexion realizada al servidor POP3 "+server);
+
+            if (!pop3.login(username,password))
+                System.out.println("Error en el login");
+             else{
+                    POP3MessageInfo[] men = pop3.listMessages();
+
+                    if (men == null)
+                        System.out.println("Imposible recuperar mensajes");
+                    else
+                        System.out.println("Nº de mensajes: "+men.length);
+
+                    pop3.logout();
+                }
+
+
+        }catch (IOException e){e.printStackTrace();}
+
+        }
+    }
